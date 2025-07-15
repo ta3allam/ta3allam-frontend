5f3d1a2c53c8b2c2953e6bf8847a4912c1b30e05
MHA <98241106+MHA963@users.noreply.github.com>
2025-07-15 17:44:37 +0200

    navbar and color theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 p-4 text-white">
+    <nav className="bg-[#3B8C51] p-4 text-white">
       <div className="container mx-auto flex justify-between">
         <Link href="/" className="text-xl font-bold">Ta3allam</Link>
+        <img src="/src/app/Ellipse 1.svg" alt="Ellipse" />
+
         <div className="space-x-4">
           <Link href="/courses">Courses</Link>
           <Link href="/dashboard">Dashboard</Link>
